89a88aaa3772922e25ac1e6a66e24c1df6246750
Austin Rutherford <austinrutherford@gmail.com>
2018-01-08 21:41:30 -0600

    Delete dwuser.sh

--- columnstore/dwuser.sh
+++
@@ -1,7 +0,0 @@
-create user 'dwuser'@'%' identified by 'password';
-grant SELECT on mysql.user to 'dwuser'@'%';
-GRANT SELECT ON mysql.db TO 'dwuser'@'%';
-GRANT SELECT ON mysql.tables_priv TO 'dwuser'@'%';
-GRANT SHOW DATABASES ON *.* TO 'dwuser'@'%';
-GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'dwuser'@'%';
-GRANT ALL ON infinidb_vtable.* TO 'dwuser'@'%';
